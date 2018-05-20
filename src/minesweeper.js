86a3d98d7374403b2e9502a91bd14f0edf89c5b1
alexstouffer <bizkite@hotmail.com>
2018-05-19 18:41:26 -0700

    Days 11 & 12 of Codecademy Pro BWA

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,22 +1,49 @@
-//Create Game Board
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    let board = [];
+    for(var i = 0; i < numberOfRows; i++){
+        let row = [];
+        for(var j = 0; j < numberOfColumns; j++){
+            row.push(' ');
+        }
+        board.push(row);
+    }
+
+    return board;
+};
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    let board = [];
+    for(var i = 0; i < numberOfRows; i++){
+        let row = [];
+        for(var j = 0; j < numberOfColumns; j++){
+            row.push(null);
+        }
+        board.push(row);
+    }
+
+    let numberOfBombsPlaced = 0;
+    while(numberOfBombsPlaced < numberOfBombs){
+        let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+        if(board[randomRowIndex][randomColumnIndex] !== 'B'){
+            board[randomRowIndex][randomColumnIndex] = 'B';
+            numberOfBombsPlaced++
+        }
+    }
+    
+    return board;
+};
+
 const printBoard = board => {
-    console.log('Current Board:');
-    console.log(board[0].join(' | '));
-    console.log(board[1].join(' | '));
-    console.log(board[2].join(' | '));
-}
-const board = [
-    [' ',' ',' '], 
-    [' ',' ',' '], 
-    [' ',' ',' '],
-];
+    console.log(board.map(row => row.join(' | ')).join('\n'));
+};
 
-//Print Current Game Board
-printBoard(board);
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
 
-//Set Values On Game Board
-board[0][1] = '1';
-board[2][2] = 'B';
+console.log('Player Board: ');
+printBoard(playerBoard);
 
-//Print Board with Set Values
-printBoard(board);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
+
